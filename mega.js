d464cbd805b6e017ba069002b45343e0e10195cf
Nick-jpk <nickheavens52@gmail.com>
2026-02-11 12:04:59 +0300

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,5 +1,4 @@
 const mega = require("megajs");
-
 const auth = {
   email: "opakstg@gmail.com",
   password: "sweetsixteen",
@@ -7,34 +6,41 @@ const auth = {
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
 
-async function upload(data, name) {
+const upload = (data, name) => {
   return new Promise((resolve, reject) => {
     const storage = new mega.Storage(auth);
 
-    let cleanedUp = false;
-    function cleanup() {
-      if (!cleanedUp) {
-        cleanedUp = true;
-        storage.close();
-      }
-    }
-
+    // Wait for storage to be ready
     storage.on("ready", () => {
-      console.log("Mega storage ready. Uploading file:", name);
+      console.log("Storage is ready. Proceeding with upload.");
 
       const uploadStream = storage.upload({ name, allowUploadBuffering: true });
 
       uploadStream.on("complete", (file) => {
         file.link((err, url) => {
-          cleanup();
-          if (err) return reject(err);
-          resolve(url);
+          if (err) {
+            reject(err);
+          } else {
+            storage.close();
+            resolve(url);
+          }
         });
       });
 
       uploadStream.on("error", (err) => {
-        cleanup();
         reject(err);
+      });
+
+      data.pipe(uploadStream);
+    });
+
+    storage.on("error", (err) => {
+      reject(err);
+    });
+  });
+};
+
+module.exports = { upload };        reject(err);
       });
 
       data.pipe(uploadStream);
